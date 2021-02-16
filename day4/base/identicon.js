9bdf5ea5332c09a0fa9a8c6a3b13a2df84623a9b
kuribara_s <kuribara_s@works-hi.co.jp>
2021-02-16 22:06:25 +0900

    day5

--- day4/base/identicon.js
+++ day4/base/identicon.js
@@ -1,3 +1,9 @@
 $(function () {
     /* Implement here */
+    $("#generate_button").on('click', function () {
+        var name = $("#user_name").val();
+        console.log(name);
+        var hashName = md5(name);
+        console.log(hashName);
+    })
 });
