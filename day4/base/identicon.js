8e20020f6f4757d7ba67ca9a842a6067569d73fc
kuribara_s <kuribara_s@works-hi.co.jp>
2021-02-17 22:40:52 +0900

    day6-2

--- day4/base/identicon.js
+++ day4/base/identicon.js
@@ -6,18 +6,26 @@ $(function () {
         var hashName = md5(name);
         console.log(hashName);
 
-        var hashList = {};
-        for (i = 0; i < hashName.length(); i++) {
-            hashList.add(hashName.substring(i, i + 1));
-        }
+        var hashList = hashName.split('');
         console.log(hashList);
 
-        var hashArray = [[false, false, false, false, false],
-        [false, false, false, false, false],
-        [false, false, false, false, false],
-        [false, false, false, false, false],
-        [false, false, false, false, false]];
+        var hashArray = [
+            [false, false, false, false, false],
+            [false, false, false, false, false],
+            [false, false, false, false, false],
+            [false, false, false, false, false],
+            [false, false, false, false, false]
+        ];
         console.log(hashArray);
 
+        for (var y = 0; y < 3; y++) {
+            for (var x = 0; x < 5; x++) {
+                if (parseInt(hashList[x + 3 * y], 16) % 2 == 0) {
+                    hashArray[x][y] = true;
+                    hashArray[4 - x][y] = true;
+                }
+            }
+        }
+        console.log(hashArray);
     })
 });
