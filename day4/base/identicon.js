8544d92813c5749d60ead61fa5e8692ff838c797
kuribara_s <kuribara_s@works-hi.co.jp>
2021-02-18 22:15:14 +0900

    day6

--- day4/base/identicon.js
+++ day4/base/identicon.js
@@ -18,8 +18,8 @@ $(function () {
         ];
         console.log(hashArray);
 
-        for (var y = 0; y < 3; y++) {
-            for (var x = 0; x < 5; x++) {
+        for (let y = 0; y < 5; y++) {
+            for (let x = 0; x < 3; x++) {
                 if (parseInt(hashList[x + 3 * y], 16) % 2 == 0) {
                     hashArray[x][y] = true;
                     hashArray[4 - x][y] = true;
@@ -27,5 +27,19 @@ $(function () {
             }
         }
         console.log(hashArray);
+
+        const cvs = $('#identicon').get(0);
+        const ctx = cvs.getContext('2d');
+        const cellWidth = 100;
+        const cellHeight = 100;
+        ctx.fillStyle = "orange";
+        ctx.clearRect(0, 0, 500, 500);
+        for (let y = 0; y < 5; y++) {
+            for (let x = 0; x < 5; x++) {
+                if (hashArray[x][y]) {
+                    ctx.fillRect(x * 100, y * 100, cellWidth, cellHeight);
+                }
+            }
+        }
     })
 });
