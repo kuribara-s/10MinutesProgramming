3886ecc1e9c742e4c1d923f799408014fa4a303e
kuribara_s <kuribara_s@works-hi.co.jp>
2021-02-19 22:32:43 +0900

    day8

--- day4/base/identicon.js
+++ day4/base/identicon.js
@@ -28,11 +28,19 @@ $(function () {
         }
         console.log(hashArray);
 
+        const digit26to28 = hashList.slice(25, 28);
+        const digit29to30 = hashList.slice(28, 30);
+        const digit31to32 = hashList.slice(30);
+        const hue = rangeMap(parseInt(digit26to28.join(''), 16), 0, 4095, 0, 360);
+        const saturation = 65 - rangeMap(parseInt(digit29to30.join(''), 16), 0, 255, 0, 20);
+        const lightness = 75 - rangeMap(parseInt(digit31to32.join(''), 16), 0, 255, 0, 20);
+        console.log("color(hsl): ", hue, saturation, lightness);
+
         const cvs = $('#identicon').get(0);
         const ctx = cvs.getContext('2d');
         const cellWidth = 100;
         const cellHeight = 100;
-        ctx.fillStyle = "orange";
+        ctx.fillStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
         ctx.clearRect(0, 0, 500, 500);
         for (let y = 0; y < 5; y++) {
             for (let x = 0; x < 5; x++) {
@@ -42,4 +50,10 @@ $(function () {
             }
         }
     })
+
+    function rangeMap(oldValue, oldMin, oldMax, newMin, newMax) {
+        const oldRange = (oldMax - oldMin);
+        const newRange = (newMax - newMin);
+        return Math.round((((oldValue - oldMin) * newRange) / oldRange) + newMin);
+    }
 });
