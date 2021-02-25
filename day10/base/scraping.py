cd91dd444dd41d4005d52bb7c9328579ac00279d
kuribara_s <kuribara_s@works-hi.co.jp>
2021-02-25 23:05:12 +0900

    day11

--- day10/base/scraping.py
+++ day10/base/scraping.py
@@ -7,6 +7,13 @@ url = "https://cookpad.com/recipe/6311648"
 
 # Implement here.
 html = requests.get(url)
-soup = BeautifulSoup(html.comtent, "html.parser")
-nodes = soup.select("div[class*='ingredients_list']")
-print(nodes)
+soup = BeautifulSoup(html.content, "html.parser")
+ingredients = soup.select("div[class='ingredients_row']")
+print(ingredients)
+
+with open("ingredient.csv", "w") as f:
+  writer = csv.writer(f)
+  for ingredient in ingredients:
+    name = ingredient.select("div[class*=ingredient_name']")[0].text.strip()
+    quantity = ingredient.select("div[class*='ingredient_quantity']")[0].text.strip()
+    writer.writerow([name, quantity])
