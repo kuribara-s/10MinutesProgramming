8093c99f326c2f63c576300f66343b80c799ede1
kuribara_s <kuribara_s@works-hi.co.jp>
2021-02-27 15:47:27 +0900

    day12

--- day12/base/pieChart.py
+++ day12/base/pieChart.py
@@ -3,10 +3,31 @@ from bs4 import BeautifulSoup
 import datetime
 import matplotlib.pyplot as plt
 
-url = "https://github.com/wakwak1704" #Change me!
+url = "https://github.com/kuribara-s" #Change me!
 html = requests.get(url)
 soup = BeautifulSoup(html.content, "html.parser")
-nodes = soup.select(
+contributionCalendars = soup.select(
     "div[class*='ContributionCalendar'] svg g g rect[class*='ContributionCalendar-day']")
 
+totalDaysOfEachLevel = [0,0,0,0,0]
 # Implement here.
+for contributionCalendar in contributionCalendars:
+    date = contributionCalendar['data-date']
+    level = contributionCalendar['data-level']
+    if date >= "2021-02-01" and date <= "2021/02/29" :
+        print(date, level)
+        if level == "4":
+            totalDaysOfEachLevel[4] += 1
+        elif level == "3":
+            totalDaysOfEachLevel[3] += 1
+        elif level == "2":
+            totalDaysOfEachLevel[2] += 1
+        elif level == "1":
+            totalDaysOfEachLevel[1] += 1
+        else:
+            totalDaysOfEachLevel[0] += 1
+
+
+print(totalDaysOfEachLevel)
+plt.pie(totalDaysOfEachLevel, colors=["#e1e4e8", "#85e89d", "#34d058", "#22863a", "#144620"])
+plt.show()
